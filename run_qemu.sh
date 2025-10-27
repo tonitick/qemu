b4050c4062d38dafe7825dc612f960be31e47dd6
Zheng Zhong <tonitick@hotmail.com>
2025-10-27 03:29:27 -0400

    reorganize config

---
+++ run_qemu.sh
@@ -0,0 +1,40 @@
+# GDB may have ./.gdbinit loading disabled by default.  In that case you can
+# follow the instructions it prints.  They boil down to adding the following to
+# your home directory's ~/.gdbinit file:
+#
+#   add-auto-load-safe-path /path/to/qemu/.gdbinit
+
+# Load QEMU-specific sub-commands and settings
+# source scripts/qemu-gdb.py
+# b vcpu_insn_exec_before
+#run --plugin ./tests/tcg/plugins/libvirtual.so,monitor=../ws/monitor.elf,logger=ws/log_config.txt,virtual=ws/virtuals.txt,detour=ws/detours.txt,modifier=ws/modifiers.txt -d in_asm,op -D qemu.log -machine mps2-an385 -monitor telnet:127.0.0.1:5555,server,nowait -semihosting --semihosting-config enable=on,target=native -qmp unix:/tmp/qmp-sock,server,nowait -kernel ../ws/RTOSDemo.axf -serial stdio -nographic -gdb tcp::1235 -S
+
+# run --plugin ./tests/tcg/plugins/libvirtual.so,monitor=../ws/monitor.elf,logger=ws/log_config.txt,virtual=ws/virtuals.txt,detour=ws/detours.txt,modifier=ws/modifiers.txt -d in_asm,op -D qemu.log -machine pixhawk1 -monitor telnet:127.0.0.1:5555,server,nowait -semihosting --semihosting-config enable=on,target=native -qmp unix:/tmp/qmp-sock,server,nowait -kernel ../ws/RTOSDemo.axf -serial stdio -nographic -gdb tcp::1235 -S
+
+#run --plugin ./tests/tcg/plugins/libvirtual.so -machine mps2-an385 -monitor null -kernel ws/RTOSDemo.axf -serial stdio -nographic
+
+##### m4_debug
+# run --plugin ./tests/tcg/plugins/libvirtual.so,logger=ws/log_config.txt,virtual=ws/virtuals.txt,detour=ws/detours.txt,modifier=ws/modifiers.txt,args=ws/rand_setting.json,outs=ws/out_setting.json,basicblocks=ws/bb.txt -d in_asm,op -D qemu.log -machine pixhawk1 -monitor telnet:127.0.0.1:5555,server,nowait -semihosting --semihosting-config enable=on,target=native -qmp unix:/tmp/qmp-sock,server,nowait -kernel ../ws/m4_debug -serial stdio -nographic -gdb tcp::1235 -S
+
+##### gs3
+# ./qemu-system-arm --plugin ./tests/tcg/plugins/libvirtual.so,virtual=ws/gs3_config/virtuals.txt,detour=ws/gs3_config/detours.txt,modifier=ws/gs3_config/modifiers.txt,args=ws/gs3_config/rand_setting.json,outs=ws/gs3_config/out_setting.json,basicblocks=ws/gs3_config/bb.txt,dump_path=../abc \
+#     -d in_asm,op -D qemu.log -qmp unix:/tmp/qmp-sock,server,nowait \
+#     -machine cortexm,memory-backend=ram0 \
+#     -object memory-backend-file,id=ram0,mem-path=/dev/shm/my_m4_ram3_zz,size=512M,share=on -object memory-backend-file,id=ram1,mem-path=/dev/shm/my_m4_ram_zz,size=512K,share=on \
+#     -global cortexm-soc.shram_backend=ram1  -global cortexm-soc.ram_baseaddr=0x20000000  -global cortexm-soc.shram_baseaddr=0x30000000 \
+#     -cpu cortex-m7 -device loader,file=../ws/gs3.bin,addr=0x08000000 -global armv7m.init-nsvtor=0x08000000 \
+#     -serial stdio -nographic -gdb tcp::1235 -S
+
+##### rover2
+firmware_bin_path=../ws/rover2.bin
+firmware_vtor_table_addr=0x08100000
+tcp_port=1235
+# ws_dir=ws/ngc2_avg_config
+ws_dir=ws/ngc2_fusion_config
+./qemu-system-arm --plugin tests/tcg/plugins/libvirtual.so,virtual=${ws_dir}/virtuals.txt,modifier=${ws_dir}/modifier.txt,args=${ws_dir}/rand_setting.json,outs=${ws_dir}/out_setting.json,basicblocks=${ws_dir}/bb.txt,function_starts=${ws_dir}/function_start.txt,function_ends=${ws_dir}/function_ends.txt,dump_path=${ws_dir}/collected_data \
+    -d in_asm,op -D qemu.log -qmp unix:/tmp/qmp-sock,server,nowait \
+    -machine cortexm,memory-backend=ram0 \
+    -object memory-backend-file,id=ram0,mem-path=/dev/shm/my_m4_ram3_zz,size=512M,share=on -object memory-backend-file,id=ram1,mem-path=/dev/shm/my_m4_ram_zz,size=512K,share=on \
+    -global cortexm-soc.shram_backend=ram1  -global cortexm-soc.ram_baseaddr=0x20000000  -global cortexm-soc.shram_baseaddr=0x30000000 \
+    -cpu cortex-m7 -device loader,file=${firmware_bin_path},addr=0x08000000 -global armv7m.init-nsvtor=${firmware_vtor_table_addr} \
+    -serial stdio -nographic -gdb tcp::${tcp_port} -S 2>&1 | tee log
